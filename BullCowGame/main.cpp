22121e7725c79b9015806328668f78abc1b1262e
bogdan-marian <bogdan.oloeriu@gmail.com>
2019-05-31 10:30:46 +0200

    get guess

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main(void) {
+void PrintIntro();
+string GetGuessAndPrintBack();
 
-     cout << "Hello World" << endl;
+// the entry point for our application
+int main()
+{
+	PrintIntro();
 
-     return(0);
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
 
+	cout << endl;
+	return 0;
+}
+
+// introduce the game
+void PrintIntro() {
+	constexpr int WORLD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+// get a guess from the player
+string GetGuessAndPrintBack() {
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	// print the guess back
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
 }
