e6d19f5d84a1a4794695236ed799c598352b25b8
Bogdan <bogdan.oloeriu@gmail.com>
2019-06-09 17:56:48 +0200

    C12 booleans and comparisons

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -4,22 +4,24 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+bool AskToPlayAgain();
+string GetGuess();
+
 
 // the entry point for our application
 int main()
 {
 	PrintIntro();
-
-	GetGuessAndPrintBack();
-	GetGuessAndPrintBack();
-
-	cout << endl;
-	return 0;
+	PlayGame();
+	AskToPlayAgain();
+	return 0; // exit the application
 }
 
+
 // introduce the game
-void PrintIntro() {
+void PrintIntro()
+{
 	constexpr int WORLD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORLD_LENGTH;
@@ -28,13 +30,32 @@ void PrintIntro() {
 	return;
 }
 
-// get a guess from the player
-string GetGuessAndPrintBack() {
+
+void PlayGame()
+{
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 1;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+
+string GetGuess()
+{
+	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-
-	// print the guess back
-	cout << "Your guess was: " << Guess << endl;
 	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again? ";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
